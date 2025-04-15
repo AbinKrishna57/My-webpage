e879997e35233e506194d7db5533bad29484a36a
Abin Krishna <abinkrishna57@gmail.com>
2025-04-15 18:36:12 +0530

    create task added and some animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
-function add_button(){
-  document.getElementById('spacing').style.color='red';
-}
+const addbtn=document.getElementById("add-btn");
+const space=document.querySelector(".spacing");
+
+addbtn.addEventListener("click", ()=>{
+  const new_task=document.createElement("div");
+  const name=document.createElement("label");
+  const inp=document.createElement("textarea");
+  const br=document.createElement("br");
+  const del=document.createElement("button");
+  const save=document.createElement("button");
+
+  new_task.classList.add("animate__animated", "animate__zoomInDown", "animate__fast", "task-container");
+  name.id="n";
+  inp.id="i";
+  save.id="save-btn";
+  del.id="delete-btn";
+
+  del.innerHTML="Delete";
+  save.innerHTML="Save";
+  name.innerHTML="Task:<br>";
+  inp.rows="1";
+  inp.placeholder=" Enter your task...."
+
+  new_task.appendChild(name);
+  new_task.appendChild(inp);
+  new_task.appendChild(br);
+  new_task.appendChild(save);
+  new_task.appendChild(del);
+  space.appendChild(new_task);
+
+  inp.addEventListener("input", ()=>{
+    inp.style.height="auto";
+    inp.style.height=inp.scrollHeight+"px";
+  });
+
+  del.addEventListener("click", ()=>{
+    new_task.classList.add("animate__zoomOutDown", "animate__faster");
+    new_task.addEventListener("animationend", ()=>{
+      new_task.remove();
+    });
+  });
+
+  save.addEventListener("click", ()=>{
+    const taskText=inp.value;
+    const t=document.getElementById("tasks");
+    if(t && taskText!=""){
+      t.innerHTML+=taskText+"<br>";
+    }
+    
+    new_task.classList.add("animate__fadeInDown");
+    new_task.addEventListener("animationend", ()=>{
+      new_task.remove();
+    });
+  });
+});
